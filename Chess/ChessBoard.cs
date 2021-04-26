29718ed0db4642f51c8bd8876e7c8133ddc0f11c
JeffHason <67012500+KyberJeffHason@users.noreply.github.com>
2021-04-26 23:27:39 +0300

    Added method for specifying width and height of chess board.

--- Chess/ChessBoard.cs
+++ Chess/ChessBoard.cs
@@ -9,10 +9,17 @@ namespace Chess
     class ChessBoard
     {
         public static int Size;
+        public static int Size_2;
 
         public ChessBoard(int size) // Constructor
         {
             Size = size;
+        }
+
+        public ChessBoard(int size, int size_2) // Second constructor 
+        {
+            Size = size;
+            Size_2 = size_2;
         }
 
         public void WriteBoard() // First OverLoad
@@ -100,6 +107,102 @@ namespace Chess
                 }
             }
         }
+
+        public void WriteBoard(bool СonstUse) // Third overload
+        {
+            if (СonstUse == true)
+            {
+                string board1 = "";
+                string board2 = "";
+
+                for (int f = 1; f < Size + 1; f++)
+                {
+                    if (f % 2 != 0)
+                    {
+                        board1 += "#";
+                    }
+                    else
+                    {
+                        board1 += ".";
+                    }
+                }
+
+                for (int d = 1; d < Size + 1; d++)
+                {
+                    if (d % 2 != 0)
+                    {
+                        board2 += ".";
+                    }
+                    else
+                    {
+                        board2 += "#";
+                    }
+                }
+
+                for (int i = 0; i < Size_2; i++)
+                {
+
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(board1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(board2);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have disabled the Constructor Usage, use other methods or enable it!");
+            }
+        }
+
+
+
+        public static void WriteBoard(int size_of_board, int size_2_of_board) // Fourth overload
+        {
+            string board1 = "";
+            string board2 = "";
+
+            for (int f = 1; f < size_of_board + 1; f++)
+            {
+                if (f % 2 != 0)
+                {
+                    board1 += "#";
+                }
+                else
+                {
+                    board1 += ".";
+                }
+            }
+
+            for (int d = 1; d < size_of_board + 1; d++)
+            {
+                if (d % 2 != 0)
+                {
+                    board2 += ".";
+                }
+                else
+                {
+                    board2 += "#";
+                }
+            }
+
+            for (int i = 0; i < size_2_of_board; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(board1);
+                }
+                else
+                {
+                    Console.WriteLine(board2);
+                }
+            }
+        }
+
 
     }
 }
--- Chess/Program.cs
+++ Chess/Program.cs
@@ -10,10 +10,17 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            ChessBoard ch = new ChessBoard(8); // Creating ChessBoard object
+            ///*
+            ChessBoard ch = new ChessBoard(8); // Creating ChessBoard object (in this example board will be 8x8)
             ch.WriteBoard(); // Writing out chess board named "ch".
+            ChessBoard.WriteBoard(8); // Second method for writing out Chess Board.
+            //*/
 
-            ChessBoard.WriteBoard(5); // Second method for writing out Chess Board.
+            ///*
+            ChessBoard ch1 = new ChessBoard(8, 20); // Also you can specify the width and height of the board. 8 is width and 20 is height. 
+            ch1.WriteBoard(true); // For writing out the board we created above you need to set argument here to "true".
+            ChessBoard.WriteBoard(8, 3); // Or you can write out the same chess board above without creating new object.
+            //*/
         }
     }
 }
